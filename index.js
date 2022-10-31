44048db7ab1331e7be48aa8a70cd77bf6a31cb7b
Nandini Vishwakarma <91829865+nandinist@users.noreply.github.com>
2022-10-31 17:06:12 +0530

    fix - imported code from repl.it

--- index.js
+++ index.js
@@ -2,73 +2,90 @@ var readlineSync = require("readline-sync");
 
 var score = 0;
 
-// data of high score
 var highScores = [
   {
-    name: "Nandini",
-    score: 3,
+    name: "Shivam",
+    score: 4,
   },
 
   {
-    name: "Akash",
+    name: "Shivani",
     score: 2,
   },
-];
+  {
+    name: "Ananya",
+    score: 6,
+  }
+]
 
-// array of objects
-var questions = [
-  {
-    question: "Where do I live? ",
-    answer: "Varanasi",
-  },
-  {
-    question: "My favorite superhero would be? ",
-    answer: "Spider Man",
-  },
-  {
-    question: "Where do I work? ",
-    answer: "Student",
-  },
-];
+
+var questions = [{
+  question: "Where do I live? ",
+  answer: "Varanasi"
+}, {
+  question: "My favorite color would be? ",
+  answer: "Black"
+},
+{
+  question: "My favorite subject is ________ ",
+  answer: "Science"
+},
+{
+  question: "What I like to do? ",
+  answer: "Learn different languages"
+},
+{
+  question: "My hobby is _______ ",
+  answer: "Reading"
+},
+{
+  question: "My favorite anime would be? ",
+  answer: "Death Note"
+},
+{
+  question: "My favorite genre of books would be? ",
+  answer: "Fictional"
+}];
 
 function welcome() {
   var userName = readlineSync.question("What's your name? ");
 
-  console.log("Welcome " + userName + " to DO YOU KNOW Tanay?");
+  console.log("Welcome " + userName + " to DO YOU KNOW Nandini?");
 }
 
-// play function
+
+
 function play(question, answer) {
   var userAnswer = readlineSync.question(question);
 
   if (userAnswer.toUpperCase() === answer.toUpperCase()) {
-    // branching
     console.log("right!");
     score = score + 1;
+
   } else {
     console.log("wrong!");
+
   }
 
   console.log("current score: ", score);
-  console.log("-------------");
+  console.log("-------------")
 }
 
 function game() {
   for (var i = 0; i < questions.length; i++) {
     var currentQuestion = questions[i];
-    play(currentQuestion.question, currentQuestion.answer);
+    play(currentQuestion.question, currentQuestion.answer)
   }
 }
 
 function showScores() {
-  console.log("YAY! You SCORED: ", score);
+  console.log("Your Score: ", score);
 
-  console.log(
-    "Check out the high scores, if you should be there ping me and I'll update it"
-  );
+  console.log("Check out the high scores");
 
-  highScores.map((score) => console.log(score.name, " : ", score.score));
+  highScores.map(score => console.log(score.name, " : ", score.score))
 }
+
 
 welcome();
 game();
